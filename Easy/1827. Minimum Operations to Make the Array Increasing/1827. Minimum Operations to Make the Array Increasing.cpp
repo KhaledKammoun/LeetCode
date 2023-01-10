3cd3bb14263f81bfe4ac9046d734729bb3f31fc0
KhaledKammoun <khaledsfax03@gmail.com>
2023-01-10 13:55:47 +0100

    C++

---
+++ Easy/1827. Minimum Operations to Make the Array Increasing/1827. Minimum Operations to Make the Array Increasing.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int som = 0;
+        for (int i = 0 ;i< nums.size()-1 ; i++){
+            if (nums[i]>=nums[i+1]){
+                som+=nums[i]+1-nums[i+1] ;
+                nums[i+1]=nums[i]+1 ;
+            }
+        }
+        return som ;
+    }
+};
