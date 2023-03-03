532a07eab8de2ddcf66c734bbe5e742afcf44080
KhaledKammoun <khaledsfax03@gmail.com>
2023-03-04 00:16:39 +0100

    C++

--- Easy/20. Valid Parentheses/20. Valid Parentheses.cpp
+++ Easy/20. Valid Parentheses/20. Valid Parentheses.cpp
@@ -1,30 +1,17 @@
 class Solution {
 public:
     bool isValid(string s) {
-    int n = s.length() ;
-    vector <char>v {s[0]} ;
-    for (int i = 1 ; i<n ; i++){
-        if (v.size()>0){
-            if (s[i]=='(' || s[i]=='[' || s[i]=='{')
-                v.push_back(s[i]) ;
-            else {
-                int code = (int)s[i] ;
-                if (s[i]==')')
-                    code-- ;
-                else
-                    code-=2 ;
-                if (((int)v[v.size()-1])==code)
-                    v.pop_back() ;
-                else
-                    return false ;
-            }
+        stack<char>stc ;
+        
+        for (int i = 0 ; i<s.size() ;i++){
+            if (!stc.empty() && ((s[i] == '}' && stc.top() == '{') || 
+                                 (s[i] == ')' && stc.top() == '(') || 
+                                 (s[i] == ']' && stc.top() == '[')))
+                stc.pop() ;
+            else 
+                stc.push(s[i]) ;
+            
         }
-        else 
-            v.push_back(s[i]) ;
-    }
-    if (v.size()>0)
-        return false ;
-    else 
-        return true ;
+        return stc.empty() ;
     }
 };
