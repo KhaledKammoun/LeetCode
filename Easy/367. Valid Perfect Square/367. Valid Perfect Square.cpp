b29bd57e0dd425c4ebef62f455df2919105db3e2
KhaledKammoun <khaledsfax03@gmail.com>
2023-03-25 14:44:16 +0100

    C++

--- Easy/367. Valid Perfect Square/367. Valid Perfect Square.cpp
+++ Easy/367. Valid Perfect Square/367. Valid Perfect Square.cpp
@@ -3,7 +3,7 @@ public:
     bool isPerfectSquare(int num) {
         float f = pow(num,0.5) ;
         if ((f-(int)f)!=0)
-            return 0 ;
+            return false ;
         
         return (f*f)==num ;
     }
