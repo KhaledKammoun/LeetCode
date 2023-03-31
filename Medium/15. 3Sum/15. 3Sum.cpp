b7d2d7a8acf6ac2eb429af804917d064694d22a2
KhaledKammoun <khaledsfax03@gmail.com>
2023-03-31 18:30:51 +0100

    C++

--- Medium/15. 3Sum/15. 3Sum.cpp
+++ Medium/15. 3Sum/15. 3Sum.cpp
@@ -3,9 +3,10 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(),nums.end()) ;
         vector<vector<int>>v ;
-        unordered_map<string, bool> m;
         int n = nums.size() ;
         for (int i = 0 ; i<n-2 ; i++){
+            if (i>0 && nums[i-1]==nums[i])
+                continue ;
             int j = i+1 , k=n-1;
             while (j<k){
                 int sum = nums[i] + nums[j] + nums[k];
@@ -14,12 +15,11 @@ public:
                 else if (sum>0)
                     k--;
                 else {
-                    string key = to_string(nums[i]) + "," + to_string(nums[j]) + "," + to_string(nums[k]);
-                    if (!m[key]){
                     v.push_back({nums[i], nums[j], nums[k]});
-                    m[key] = true;}
                     j++;
                     k--;
+                    while (j<n-1 && nums[j]==nums[j-1])
+                        j++ ;
                 }
             }
         }
