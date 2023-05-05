c032c858c8f4404244fb282171405832a8cf1f68
KhaledKammoun <khaledsfax03@gmail.com>
2023-05-05 23:41:29 +0100

    C++

--- Hard/76. Minimum Window Substring/76. Minimum Window Substring.cpp
+++ Hard/76. Minimum Window Substring/76. Minimum Window Substring.cpp
@@ -1,48 +1,47 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int n_s = s.size(), n_t = t.size() ;
-        if (n_s<n_t)
-            return "" ;
-        else {
-            
-            int tab_s[100]={0}, tab_t[100]={0} ;
-            for (int i = 0 ; i<n_t ; i++)
-                tab_t[t[i]-'A']++ ;
-
-            string s_min = "",s_var="" ;
-            s_var.append(1,s[0]) ;
-            
-            int i = 0, j = 0, c = 0 ;
-            tab_s[s[0]-'A']++ ;
-            if (tab_s[s[0]-'A']==tab_t[s[0]-'A']){
-                c+=tab_s[s[0]-'A'] ;
-            }
-            while (i<n_s){
-                if (c>=n_t || j==n_s-1){
-                    if ((s_min=="" && c>=n_t) || (c>=n_t && s_min.size()>s_var.size())){
-                        s_min = s_var ;
-                    }
-                    if (tab_s[s[i]-'A']==tab_t[s[i]-'A'])
-                        c-=tab_s[s[i]-'A'] ;
-                    tab_s[s[i]-'A']-- ;
-                    s_var.erase(s_var.begin()) ;
-                    i++ ;
-                }else {
-                    j++ ;
-                    tab_s[s[j]-'A']++ ;
-                    if (tab_s[s[j]-'A']==tab_t[s[j]-'A']){
-                        c+=tab_s[s[j]-'A'] ;
-                    }
-                    s_var.append(1,s[j]) ;
-                }
-                
-            }
-            if (n_t==1 && t[0]!=s[0] && n_s==1)
-                return "" ;
-            return s_min ;
+        if (t.empty() || s.empty()) {
+            return "";
         }
-       
-        
+
+        unordered_map<char, int> tab_t, tab_s;
+        for (char c : t) {
+            tab_t[c]++;
+        }
+
+        int l = 0, ma = 0, len = INT_MAX;
+        pair<int, int> p;
+
+        for (int r = 0; r < s.size(); r++) {
+            if (tab_t.count(s[r])) {
+                tab_s[s[r]]++;
+                if (tab_s[s[r]] == tab_t[s[r]]) {
+                    ma++;
+                }
+            }
+
+            while (ma == tab_t.size()) {
+                if (len > r - l + 1) {
+                    len = r - l + 1;
+                    p = {l, r};
+                }
+
+                if (tab_t.count(s[l])) {
+                    tab_s[s[l]]--;
+                    if (tab_s[s[l]] < tab_t[s[l]]) {
+                        ma--;
+                    }
+                }
+
+                l++;
+            }
+        }
+
+        if (len == INT_MAX) {
+            return "";
+        }
+
+        return s.substr(p.first, len);
     }
 };
