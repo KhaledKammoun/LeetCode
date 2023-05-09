ae17a19cd5157bed85cf40b0618ccb27f80da304
KhaledKammoun <khaledsfax03@gmail.com>
2023-05-09 22:43:31 +0100

    C++

--- Easy/704. Binary Search/704. Binary Search.cpp
+++ Easy/704. Binary Search/704. Binary Search.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int s = 0 , e = nums.size() -1;
-        int index = -1 ;
-        while (s<=e){
-            int m =  s+ (e-s)/2 ;
-            if (target>nums[m])
-                s = m+1 ;
-            else
-                e = m -1;
-            if (target==nums[m])
-                index = m ;
-        }
-        return index ;
+            int l = 0, r = nums.size()-1 ;
+            while (l<=r){
+                int m = l + (r-l)/2 ; //r+l : overflow
+                if (nums[m]>target)
+                    r = m-1 ;
+                else if (nums[m]<target)
+                    l = m+1 ;
+                else
+                    return m ;
+            }
+            return -1 ;
         }
 };
