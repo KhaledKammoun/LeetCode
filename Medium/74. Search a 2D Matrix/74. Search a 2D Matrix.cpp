e113c1f0600a97e5486f8dcfbcc371bac925b11c
KhaledKammoun <khaledsfax03@gmail.com>
2023-05-13 20:17:53 +0100

    C++

--- Medium/74. Search a 2D Matrix/74. Search a 2D Matrix.cpp
+++ Medium/74. Search a 2D Matrix/74. Search a 2D Matrix.cpp
@@ -28,3 +28,26 @@ public:
         return false ;
     }
 };
+/*Second method :
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& mat, int target) {
+        int m=mat.size();
+        int n=mat[0].size();
+        int start=0;
+        int end=m*n-1;
+        while(start<=end){
+            int mid = start + ((end-start)>>1) ;
+            int row = mid / n;
+            int col = mid % n;
+            if(mat[row][col]==target){
+                return true;
+            }else if(mat[row][col]>target){
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+        return false;
+    }
+};*/
