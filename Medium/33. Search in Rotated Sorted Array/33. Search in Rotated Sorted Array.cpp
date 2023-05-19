71391da4356b35085317eeb3835a9065244f536b
KhaledKammoun <khaledsfax03@gmail.com>
2023-05-19 22:58:10 +0100

    C++

---
+++ Medium/33. Search in Rotated Sorted Array/33. Search in Rotated Sorted Array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int m = (l + r) / 2;
+            if (nums[m] == target)
+                return m;
+            if (nums[m] >= nums[l]) {
+                if (target >= nums[l] && target < nums[m]) {
+                    r = m - 1;
+                } else {
+                    l = m + 1;
+                }
+            } else {
+                if (target > nums[m] && target <= nums[r]) {
+                    l = m + 1;
+                } else {
+                    r = m - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
