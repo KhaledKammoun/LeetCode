e292871b65ce0bcfe27e3e0a7dd6f8ebe2589d3a
KhaledKammoun <khaledsfax03@gmail.com>
2023-06-06 22:23:38 +0100

    C++

--- Easy/21. Merge Two Sorted Lists/21. Merge Two Sorted Lists.cpp
+++ Easy/21. Merge Two Sorted Lists/21. Merge Two Sorted Lists.cpp
@@ -10,20 +10,26 @@
  */
     class Solution {
     public:
-        ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) 
+        ListNode* mergeTwoLists(ListNode* l1, ListNode* l2)
         {
-            if (l1==NULL) 
+            ListNode* newList = new ListNode() ;
+            ListNode* var = newList;
+            if (l1==nullptr)
                 return l2 ;
-            if (l2==NULL)
+            if (l2==nullptr)
                 return l1 ;
-            
-            if (l1->val >= l2->val){
-                l2->next = mergeTwoLists(l1,l2->next) ;
-                return l2 ;
+            while (l1!=nullptr || l2!=nullptr){  
+                if (l2==nullptr || (l1!=nullptr && l1->val<l2->val)){
+                    var->next = l1 ; 
+                    l1 = l1->next ;
+                }
+                else{
+                    var->next = l2 ;
+                    l2 = l2->next ;
+                }
+                var = var->next;
             }
-            else {
-                l1->next = mergeTwoLists(l1->next,l2) ;
-                return l1 ;
-            }
+
+            return newList->next ;
         }
 };	
