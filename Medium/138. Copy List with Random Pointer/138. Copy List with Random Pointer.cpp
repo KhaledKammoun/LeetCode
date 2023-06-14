45aaa1891af5476453af871bc72c26f31ac8c4be
KhaledKammoun <khaledsfax03@gmail.com>
2023-06-14 14:05:00 +0100

    C++

--- Medium/138. Copy List with Random Pointer/138. Copy List with Random Pointer.cpp
+++ Medium/138. Copy List with Random Pointer/138. Copy List with Random Pointer.cpp
@@ -18,27 +18,20 @@ class Solution {
 public:
     Node* copyRandomList(Node* head) {
         std::unordered_map<Node*, Node*> nodeMap;
-        Node* head_copy = new Node(0) ;
-        Node* head_copy_var = head_copy ;
         Node*x = head ;
         while (x!=nullptr){
             Node* new_Node = new Node(0) ;
             new_Node->val = x->val ;
-            new_Node->next = nullptr ;
-            new_Node->random = nullptr ;
-            head_copy_var->next = new_Node ;
             nodeMap[x] = new_Node ;
             x = x->next ;
-            head_copy_var = head_copy_var->next ;
         }
-        head_copy = head_copy->next ;
-        head_copy_var = head_copy ;
         x = head ;
         while (x!=nullptr){
-            head_copy_var->random = nodeMap[x->random] ;
-            head_copy_var = head_copy_var->next ;
+            Node* copy_node = nodeMap[x] ;
+            copy_node->random = nodeMap[x->random] ;
+            copy_node->next = nodeMap[x->next] ;
             x = x->next ; 
         }
-        return head_copy ;
+        return nodeMap[head] ;
     }
 };
