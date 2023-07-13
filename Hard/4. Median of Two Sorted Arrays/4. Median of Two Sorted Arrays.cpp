24fd82f0e348f5ec1553c58bb8a408de2e9ef20b
KhaledKammoun <khaledsfax03@gmail.com>
2023-07-13 12:31:57 +0100

    C++

--- Hard/4. Median of Two Sorted Arrays/4. Median of Two Sorted Arrays.cpp
+++ Hard/4. Median of Two Sorted Arrays/4. Median of Two Sorted Arrays.cpp
@@ -1,33 +1,36 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& A, vector<int>& B) {
-        int n = A.size(), m = B.size() ;
-        int lA = 0, rA = n-1, lB = 0, rB = m-1 ;
-        while (lA <= rA){
-            int i = (lA + rA)/2, j = (lB + rB)/2 ;
-            if (i==0 || i==n-1 || (A[i+1]>=B[j] && B[j+1] > A[i])){
-                if ((n+m)%2!=0)
-                    return max(A[i],B[j]) ;
-                else{
-                    if (i<0 || i>=n){
-                        return B[j]+B[j-1] ;
-                    }
-                    else if (j<0 || j>=m){
-                        return A[i]+A[i-1] ;
-                    }
-                    else{
-                        return (A[i]+B[j])/2 ;
-                    }
-                }
-            }else if (A[i+1]<B[j]){
-                rB = j - 1 ;
-                lA = i + 1 ;
+        int n = A.size(), m = B.size();
+        if (n == 0 && m == 0)
+            return 0.0;
+        else if (n == 0 && m > 0)
+            return (m % 2 == 0) ? (B[m / 2] + B[m / 2 - 1]) / 2.0 : B[m / 2];
+        else if (n > 0 && m == 0)
+            return (n % 2 == 0) ? (A[n / 2] + A[n / 2 - 1]) / 2.0 : A[n / 2];
+
+        if (n > m)
+            return findMedianSortedArrays(B, A);
+        int l = -1, r = n , half = (n+m)/2 ;
+        while (l<=r){
+            int i = (l + r)/2 ;
+            int j =half - i - 2 ;
+            int Aleft = (i>=0) ? A[i] : INT_MIN ;
+            int Aright = (i+1<n) ? A[i+1] : INT_MAX ;
+            int Bleft = (j>=0) ? B[j] : INT_MIN ;
+            int Bright = (j+1<m) ? B[j+1] : INT_MAX ;
+            if (Aleft<=Bright && Bleft<=Aright){
+                if ((n+m)%2==0)
+                    return (double)(max(Aleft,Bleft) + min(Aright,Bright))/2 ;
+                else
+                    return (double)min(Aright,Bright) ;
             }
-            else{
-                lB = j + 1 ;
-                rA = i - 1 ;
-            }
+            else if (Aleft>Bright)
+                r = i - 1 ;
+            else
+                l = i + 1 ;
         }
         return 0.0 ;
     }
+    
 };
