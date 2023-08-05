95fcf55edaca49b2773bd9cf96d394cdcc504d04
KhaledKammoun <khaledsfax03@gmail.com>
2023-08-05 15:05:01 +0100

    C++

--- Medium/1448. Count Good Nodes in Binary Tree/1448. Count Good Nodes in Binary Tree.cpp
+++ Medium/1448. Count Good Nodes in Binary Tree/1448. Count Good Nodes in Binary Tree.cpp
@@ -11,22 +11,39 @@
  */
 class Solution {
 public:
-    void notgoodNodes(TreeNode* root, int &good,  int max_val,bool test) {
+    void notgoodNodes(TreeNode* root, int &good,  int max_val) {
         if (!root)
             return;
         else if (root->val>=max_val){
             good++ ;
             max_val = max(max_val, root->val) ;
         }
-        notgoodNodes(root->left, good, max_val,test);
-        notgoodNodes(root->right, good, max_val, test);
+        notgoodNodes(root->left, good, max_val);
+        notgoodNodes(root->right, good, max_val);
     }
 
     int goodNodes(TreeNode* root) {
         if (!root)
             return 0;
         int good = 0 ;
-        notgoodNodes(root, good ,root->val, true);
+        notgoodNodes(root, good ,root->val);
         return good;
+    }
+};
+/*Second_Method*/
+
+class Solution {
+public:
+    int notgoodNodes(TreeNode* root,  int max_val) {
+        if (!root)
+            return 0 ;
+        max_val = max(max_val, root->val) ;
+        return ((root->val>=max_val) ? 1 : 0) + notgoodNodes(root->left, max_val) + notgoodNodes(root->right, max_val);
+    }
+
+    int goodNodes(TreeNode* root) {
+        if (!root)
+            return 0;
+        return notgoodNodes(root, root->val);
     }
 };
