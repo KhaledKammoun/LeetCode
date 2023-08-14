7d4bbf4f2182b11ec0954fab326e183f319313c2
KhaledKammoun <khaledsfax03@gmail.com>
2023-08-14 18:32:12 +0100

    C++

--- Medium/105. Construct Binary Tree from Preorder and Inorder Traversal/105. Construct Binary Tree from Preorder and Inorder Traversal..cpp
+++ Medium/105. Construct Binary Tree from Preorder and Inorder Traversal/105. Construct Binary Tree from Preorder and Inorder Traversal..cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
-    vector<int> subVector(vector<int>v, int start, int end){
-        vector<int> newVec ;
-        for (int i = start ; i<end ; i++)
-            newVec.push_back(v[i]) ;
-        return newVec ;
+    vector<int> subVector(const vector<int>& v, int s, int e) {
+        vector<int> newV;
+        if (s >= e || v.empty())
+            return newV;
+        for (int i = s; i < e; i++)
+            newV.push_back(v[i]);
+        return newV;
     }
+    
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        if (!preorder.size() || !inorder.size())
+        if (preorder.empty() || inorder.empty())
             return nullptr;
-        
+
         TreeNode* root = new TreeNode(preorder[0]);
-        
         int mid = 0;
-        for (mid = 0; mid < inorder.size(); mid++) {
-            if (inorder[mid] == preorder[0])
+        for (int i = 0; i < inorder.size(); i++) {
+            if (inorder[i] == preorder[0]) {
+                mid = i;
                 break;
+            }
         }
-        
-        root->left = buildTree(
-            subVector(preorder,1, mid + 1),
-            subVector(inorder, 0,mid)
-        );
-        
-        root->right = buildTree(
-            subVector(preorder, mid + 1,preorder.size()),
-            subVector(inorder,mid,inorder.size())
-        );
-        
+
+        vector<int> subVect = subVector(preorder, 1, mid + 1);
+        vector<int> subVect1 = subVector(inorder, 0, mid);
+        root->left = buildTree(subVect, subVect1);
+
+        subVect = subVector(preorder, mid + 1, preorder.size());
+        subVect1 = subVector(inorder, mid + 1, inorder.size());
+        root->right = buildTree(subVect, subVect1);
+
         return root;
     }
 };
