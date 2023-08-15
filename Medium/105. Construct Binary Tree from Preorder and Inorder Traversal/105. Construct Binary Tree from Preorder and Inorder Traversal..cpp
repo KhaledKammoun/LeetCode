d4240f1d3e490ed939678dd9c0b03540e585d599
KhaledKammoun <khaledsfax03@gmail.com>
2023-08-15 13:55:22 +0100

    C++

--- Medium/105. Construct Binary Tree from Preorder and Inorder Traversal/105. Construct Binary Tree from Preorder and Inorder Traversal..cpp
+++ Medium/105. Construct Binary Tree from Preorder and Inorder Traversal/105. Construct Binary Tree from Preorder and Inorder Traversal..cpp
@@ -1,35 +1,21 @@
 class Solution {
 public:
-    vector<int> subVector(const vector<int>& v, int s, int e) {
-        vector<int> newV;
-        if (s >= e || v.empty())
-            return newV;
-        for (int i = s; i < e; i++)
-            newV.push_back(v[i]);
-        return newV;
+    TreeNode* BuildNewTree(vector<int> &preorder, vector<int> &inorder, int preStart, int preEnd, int inStart, int inEnd){
+        if (preStart > preEnd || inStart > inEnd)
+            return nullptr ;
+        TreeNode* root = new TreeNode(preorder[preStart]);
+        int mid = inStart;
+            while (inorder[mid] != preorder[preStart])
+                mid++;
+
+        int leftSubtreeSize = mid - inStart;
+
+    root->left = BuildNewTree(preorder, inorder, preStart + 1, preStart + leftSubtreeSize, inStart, mid - 1);
+    root->right = BuildNewTree(preorder, inorder, preStart + leftSubtreeSize + 1, preEnd, mid + 1, inEnd);
+        return root ;
     }
-    
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        if (preorder.empty() || inorder.empty())
-            return nullptr;
-
-        TreeNode* root = new TreeNode(preorder[0]);
-        int mid = 0;
-        for (int i = 0; i < inorder.size(); i++) {
-            if (inorder[i] == preorder[0]) {
-                mid = i;
-                break;
-            }
-        }
-
-        vector<int> subVect = subVector(preorder, 1, mid + 1);
-        vector<int> subVect1 = subVector(inorder, 0, mid);
-        root->left = buildTree(subVect, subVect1);
-
-        subVect = subVector(preorder, mid + 1, preorder.size());
-        subVect1 = subVector(inorder, mid + 1, inorder.size());
-        root->right = buildTree(subVect, subVect1);
-
-        return root;
+        int n = preorder.size() ;
+        return BuildNewTree(preorder, inorder, 0,n-1, 0, n-1) ;
     }
 };
