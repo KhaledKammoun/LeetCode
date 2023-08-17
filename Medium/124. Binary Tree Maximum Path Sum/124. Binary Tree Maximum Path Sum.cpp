14d174206000dc2c82910a898b444a724363e7ad
KhaledKammoun <khaledsfax03@gmail.com>
2023-08-17 20:21:43 +0100

    C++

--- Medium/124. Binary Tree Maximum Path Sum/124. Binary Tree Maximum Path Sum.cpp
+++ Medium/124. Binary Tree Maximum Path Sum/124. Binary Tree Maximum Path Sum.cpp
@@ -11,23 +11,25 @@
  */
 class Solution {
 public:
-    int maxSum(TreeNode* root, int sum,int m){
+    void maxSum(TreeNode* root, int sum,int &m){
         if (!root)
-            return 0;
-        sum = sum + root->val ;
-        m = max(m,sum) ;
-        int x = maxSum(root->left, sum,m) ;
-        x = maxSum(root->right, sum,m) ;
-        return sum ;
-    }
-    int maxVal = INT_MIN ;
-    int maxPathSum(TreeNode* root) {
-        if (root == nullptr)
-            return 0 ;
-        maxVal = max(maxVal,max(root->val,root->val + maxSum(root->left,0,root->val) + maxSum(root->right,0,root->val))) ;
-        int x = maxPathSum(root->left) ;
-        x = maxPathSum(root->right) ;
-        return maxVal ;
+            return ;
+        sum+=root->val ;
+        m = max(m, sum) ;
+        maxSum(root->left, sum, m) ;
+        maxSum(root->right, sum, m) ;
         
+    }
+    int max_val = INT_MIN ;
+    int maxPathSum(TreeNode* root) {
+        if (!root)
+            return 0 ;
+        int left_val = 0, right_val = 0 ;
+        maxSum(root->left, 0, left_val) ;
+        maxSum(root->right, 0, right_val) ;
+        max_val = max(max_val, left_val + right_val + root->val) ;
+        int x = maxPathSum(root->left) ;
+         x = maxPathSum(root->right) ;
+        return max_val ;
     }
 };
