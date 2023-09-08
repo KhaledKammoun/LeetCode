9161d56051c5a62907fbfb5a481ca5894858d95e
KhaledKammoun <khaledsfax03@gmail.com>
2023-09-08 15:58:17 +0100

    C++

--- Hard/297. Serialize and Deserialize Binary Tree/297. Serialize and Deserialize Binary Tree_Preorder.cpp
+++ Hard/297. Serialize and Deserialize Binary Tree/297. Serialize and Deserialize Binary Tree_Preorder.cpp
@@ -25,54 +25,37 @@ public:
         serialize_preorder(root,s) ;
         return s ;
     }
-    vector<int> split(string s, char del ='#')
-    {
-        vector<int> numbers ;
-        std::stringstream ss(s);
-        std::string token;
-    
-        // Iterate through the stringstream and extract integers
-        while (std::getline(ss, token, del)) {
-            if (!token.empty()) {
-                if (token == "n") {
-                    numbers.push_back(1001); // Replace "n" with 1001
-                } else {
-                    int number;
-                    std::istringstream(token) >> number;
-                    numbers.push_back(number);
-                }
-            }
-        }
-        return numbers;
-    }
     TreeNode* deserialize(string data) {
-        vector<int>v ;
-        v = split(data) ;
-        if (v[0]==1001)
+        std::stringstream ss(data);
+        std::string token;
+        getline(ss, token, '#');
+        getline(ss, token, '#');
+        if (token=="n")
             return nullptr; 
+        int number;
+        std::istringstream(token) >> number;
         stack<TreeNode*>s ;
-        TreeNode* root = new TreeNode(v[0]) ;
+        TreeNode* root = new TreeNode(number) ;
         s.push(root) ;
-        int n = v.size() ;
-        for (int i = 1 ; i<n ; i++){
-            if (v[i]==1001 && (v[i+1]!=1001 || i+1>n)){
-                while (!s.empty()){
-                    TreeNode* node = (v[i]!=1001) ? new TreeNode(v[i]) : nullptr ;
-                    s.top()->right = node ;
-                    if (i<n && v[i]!=1001) // i<n && node != nullptr
-                        s.push(node) ;
-                    i++ ;
-                    s.pop() ;
-                }
+        string prev = token ;
+        while (std::getline(ss, token, '#')) {
+            TreeNode* node ;
+            if (token!="n"){
+                std::istringstream(token) >> number;
+                node = new TreeNode(number) ;
             }
-            
             else{
-                TreeNode* node = (v[i]!=1001) ? new TreeNode(v[i]) : nullptr ;
-                s.top()->left = node ;
+                node = nullptr ;
             }
-            TreeNode* node = (v[i]!=1001) ? new TreeNode(v[i]) : nullptr ;
-            if (i<n && v[i]!=1001)
+            if (prev=="n"){
+                s.top()->right = node ;
+                s.pop() ;
+            }
+            else
+                s.top()->left = node ;
+            if (node)
                 s.push(node) ;
+            prev = token ;
         }
         return root ;
     }
