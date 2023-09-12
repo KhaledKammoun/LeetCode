fa29df67a4a9e0328741115e666186b40e798152
KhaledKammoun <khaledsfax03@gmail.com>
2023-09-12 11:51:08 +0100

    C++

--- Medium/208. Implement Trie (Prefix Tree)/208. Implement Trie (Prefix Tree).cpp
+++ Medium/208. Implement Trie (Prefix Tree)/208. Implement Trie (Prefix Tree).cpp
@@ -1,12 +1,12 @@
 class Trie { 
 public:
-    struct Trie{
+    struct TrieNode{
         string val ;
-        Trie* left ;
-        Trie* right ;
-        Trie() : val(""), left(nullptr), right(nullptr) ;
-        Trie(string val) : val(val), left(nullptr), right(nullptr) ;
-    }
+        TrieNode* left ;
+        TrieNode* right ;
+        TrieNode() : val(""), left(nullptr), right(nullptr) {}
+        TrieNode(string val) : val(val), left(nullptr), right(nullptr) {}
+    };
     TrieNode* root;
 
     Trie() {
@@ -21,11 +21,11 @@ public:
             if (var->val[i] > word[i] || ((i == (word.size() - 1)) && var->val[i] == word[i])) {
                 var = var->left;
                 c = 'l';
-                i = 0;
+                i = -1;
             } else if (var->val[i] < word[i] || var->val.size() < word.size()) {
                 var = var->right;
                 c = 'r';
-                i = 0 ;
+                i = -1;
             } else {
                 c = 'l';
             }
@@ -41,13 +41,49 @@ public:
         if (root->val =="" && (root->left || root->right))
             root = ((!root->left) ? root->right : root->left) ;
     }
-    
-    bool search(string word) {
-        
+
+    int compare(string a, string b){
+        int a_len = a.size(), b_len = b.size() ;
+        for (int i = 0 ;i<min(a.size(),b.size()) ; i++){
+            if (a[i] > b[i])
+                return 0 ; // a > b
+            else if (a[i] < b[i])
+                return 1 ;
+        }
+        //length comparing
+        if (a_len > b_len)
+            return 0 ; // a > b
+        else if (a_len <  b_len)
+            return 1 ; // a < b
+        return 2 ; // a == b
     }
-    
+    bool search_(TrieNode* root, string word){
+        if (!root)
+            return false ;
+        int x = compare(root->val, word) ;
+        if (x == 1) // root->val < word ;
+            return search_(root->right, word) ;
+        else if (x == 0) // root->val > word
+            return search_(root->left, word) ;
+        else
+            return true ;
+    }
+    bool search(string word) {
+        return search_(root, word) ;
+    }
+    bool startsWith_(TrieNode* root, string prefix){
+        if (!root)
+            return false ;
+        int x = compare(((root->val.size()> prefix.size()) ? root->val.substr(0,prefix.size()) : root->val),prefix) ;
+        if (x == 1) // root->val < prefix ;
+            return search_(root->right, prefix) ;
+        else if (x == 0) // root->val > prefix
+            return search_(root->left, prefix) ;
+        else
+            return true ;
+    }
     bool startsWith(string prefix) {
-        
+        return startsWith_(root, prefix) ;
     }
 };
 
