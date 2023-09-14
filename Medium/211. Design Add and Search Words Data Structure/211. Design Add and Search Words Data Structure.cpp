91f18cf704419093e8780e8884c3393e7e3ed4e2
KhaledKammoun <khaledsfax03@gmail.com>
2023-09-14 14:14:09 +0100

    C++

--- Medium/211. Design Add and Search Words Data Structure/211. Design Add and Search Words Data Structure.cpp
+++ Medium/211. Design Add and Search Words Data Structure/211. Design Add and Search Words Data Structure.cpp
@@ -26,7 +26,28 @@ public:
         }
         curr->isEnd = true ;
     }
-    
+    bool search_(TrieNode* curr, string word,int index){
+        if (index == word.size())
+            return curr->isEnd ;
+        char c = word[index] ;
+        if (c =='.'){
+            // check all cases .
+            for (int i = 0 ;i<26 ; i++){
+                if (curr->children[i] && search_(curr->children[i], word,index+1))
+                    return true ;
+            }
+            // none of 26's cases have a value .
+            return false ;
+        }
+        else{
+            if (!curr->children[c-'a'])
+                return false ;
+            return search_(curr->children[c-'a'], word,index+1) ;
+        }
+    }
+    bool search(string word) {
+        return search_(root, word, 0) ;
+    }
 };
 
 /**
