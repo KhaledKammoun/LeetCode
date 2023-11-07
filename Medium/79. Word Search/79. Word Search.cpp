c9cc119d29d4c1f6147c667edf480a1e11dc5ad3
KhaledKammoun <khaledsfax03@gmail.com>
2023-11-07 22:30:33 +0100

    C++

--- Medium/79. Word Search/79. Word Search.cpp
+++ Medium/79. Word Search/79. Word Search.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
-    bool backtracking(vector<vector<char>>& board, string word, int n, int m, int i, int j, int start, vector<vector<bool>>& visited) {
+    bool backtracking(vector<vector<char>>& board, string &word, int n, int m, int i, int j, int start) {
         if (start == word.size()) {
             return true;
         }
         
-        if (i < 0 || i >= n || j < 0 || j >= m || visited[i][j] || board[i][j] != word[start]) {
+        if (i < 0 || i >= n || j < 0 || j >= m || board[i][j] == ' ' || board[i][j] != word[start]) {
             return false;
         }
         
-        visited[i][j] = true;
-        
-        if (backtracking(board, word, n, m, i + 1, j, start + 1, visited) ||
-            backtracking(board, word, n, m, i - 1, j, start + 1, visited) ||
-            backtracking(board, word, n, m, i, j + 1, start + 1, visited) ||
-            backtracking(board, word, n, m, i, j - 1, start + 1, visited)) {
+        char temp = board[i][j] ;
+        board[i][j] = ' ' ;
+        if (backtracking(board, word, n, m, i + 1, j, start + 1) ||
+            backtracking(board, word, n, m, i - 1, j, start + 1) ||
+            backtracking(board, word, n, m, i, j + 1, start + 1) ||
+            backtracking(board, word, n, m, i, j - 1, start + 1)) {
             
             return true;
         }
         
-        visited[i][j] = false;
+        board[i][j] = temp ;
         
         return false;
     }
 
     bool exist(vector<vector<char>>& board, string word) {
-        int n = board.size(), m = board[0].size();
-        vector<vector<bool>> visited(n, vector<bool>(m, false));
-        
+        int n = board.size(), m = board[0].size() ;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (backtracking(board, word, n, m, i, j, 0, visited)) {
+                if (board[i][j] == word[0] && backtracking(board, word,n,m, i, j, 0)) {
                     return true;
                 }
             }
