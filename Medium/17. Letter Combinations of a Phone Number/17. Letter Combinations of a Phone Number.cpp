0cafdf48e060344f4917ea457dc8a0db0b12eccd
KhaledKammoun <khaledsfax03@gmail.com>
2023-11-16 18:03:52 +0100

    C++

--- Medium/17. Letter Combinations of a Phone Number/17. Letter Combinations of a Phone Number.cpp
+++ Medium/17. Letter Combinations of a Phone Number/17. Letter Combinations of a Phone Number.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
-    std::unordered_map<std::string, std::string> m {
-        {"2", "abc"}, {"3", "def"}, {"4", "ghi"}, {"5", "jkl"},
-        {"6", "mno"}, {"7", "pqrs"}, {"8", "tuv"}, {"9", "wxyz"}
+    std::unordered_map<char, pair<int,char>> m {
+        {'2', make_pair(3,'a')}, {'3', make_pair(3,'d')}, {'4', make_pair(3,'g')}, {'5', make_pair(3,'j')},
+        {'6', make_pair(3,'m')}, {'7', make_pair(4,'p')}, {'8', make_pair(3,'t')}, {'9', make_pair(4,'w')}
     };
     vector<string> result ;
-    void dfs(string &s,string& sNum, int startS,int startNum){
+    void dfs(string s,string sNum, int startS, int &num){
         if (startS == s.size())
             return ;
-        for (int i = 0 ; i<m[sNum.substr(startS, 1)].size() ; i++){
+        if (result.size()!=0)
+            num = 1 ;
+        for (int i = num ; i<m[sNum[startS]].first ; i++){
+            if (i != 0)
+                s[startS]++ ;
             result.push_back(s) ;
-            dfs(s, sNum, startS + 1, startNum + 1) ;
-            s[startS]++ ;
-            
+            dfs(s, sNum, startS + 1,num) ;
+            s[startS]
         }
     }
     vector<string> letterCombinations(string digits) {
+        if (digits == "")
+            return result ;
         string s = "" ;
         for (int i = 0 ;i<digits.size() ; i++)
-            s+=m[digits.substr(i,1)][0] ;
-        dfs(s, digits, 0,0) ;
+            s+=m[digits[i]].second ;
+        int start = 0 ;
+        
+        dfs(s, digits, 0,start) ;
         return result ;
     }
 };
