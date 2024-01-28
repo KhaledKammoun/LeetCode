97c3a52bc01843398fc5111f57a8eec2ac2bffee
KhaledKammoun <khaledsfax03@gmail.com>
2024-01-28 16:07:35 +0100

    C++

--- Medium/55. Jump Game/55. Jump Game.cpp
+++ Medium/55. Jump Game/55. Jump Game.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int i = 0;
-        while (i<nums.size()-1 && nums[i]!=0){
-            if (i + nums[i] >= nums.size()-1)
-                return true ;
-            i = std::max_element(nums.begin() + i + 1, nums.begin() + i + nums[i] + 1) - nums.begin();
+        int max_jump = 0;
+        for (int i = 0; i<nums.size(); i++){
+            if (i<=max_jump){
+                max_jump = max(max_jump, i + nums[i]) ;
+            }
+            else{
+                return false ;
+            }
         }
-        return i >=nums.size() - 1 ;
+        return true ;
     }
 };
