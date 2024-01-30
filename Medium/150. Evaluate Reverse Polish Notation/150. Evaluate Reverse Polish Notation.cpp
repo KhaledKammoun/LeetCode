ffe974f0f76efee29d5421ce089bf8216cb78014
KhaledKammoun <khaledsfax03@gmail.com>
2024-01-30 23:28:44 +0100

    C++

--- Medium/150. Evaluate Reverse Polish Notation/150. Evaluate Reverse Polish Notation.cpp
+++ Medium/150. Evaluate Reverse Polish Notation/150. Evaluate Reverse Polish Notation.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        stack<int>Stack ;
-        for (int i = 0 ; i<tokens.size() ; i++){
-            if (tokens[i]!="*" && tokens[i]!="/" && tokens[i]!="-" && tokens[i]!="+")
-                Stack.push(stoi(tokens[i])) ;
-            else {
-                int som = Stack.top() ;
-                Stack.pop() ;
-                if (tokens[i]=="*")
-                    som = Stack.top() * som;
-                else if (tokens[i]=="/")
-                    som = Stack.top() / som;
-                else if (tokens[i]=="+")
-                    som = Stack.top() + som;
-                else 
-                    som = Stack.top() - som;
-                Stack.pop() ;
-                Stack.push(som) ;
+        stack<int> s ;
+        if (tokens.size() == 1)
+            return stoi(tokens[0]);
+        s.push(stoi(tokens[0])) ;
+        int i = 1 ;
+        while (i<tokens.size()){
+            if (tokens[i] == "/" || tokens[i] == "*" || tokens[i] == "+" ||tokens[i] == "-"){
+                int var = s.top() ;
+                s.pop() ;
+                int var2 = s.top() ;
+                s.pop() ;
+                if (tokens[i] == "/")
+                    s.push(var2 / var) ;
+                if (tokens[i] == "+")
+                    s.push(var2 + var) ;
+                if (tokens[i] == "*")
+                    s.push(var2 * var) ;
+                if (tokens[i] == "-")
+                    s.push(var2 - var) ;
             }
+            else{
+                s.push(stoi(tokens[i]));
+            }
+            i++ ;
         }
-        return Stack.top() ;
+        return s.top() ;
     }
 };
