d9e91ef9e90c561d368dbd0adc1100fec66936ae
KhaledKammoun <khaledsfax03@gmail.com>
2024-03-19 10:59:32 +0100

    C++

--- Medium/213. House Robber II/213. House Robber II.cpp
+++ Medium/213. House Robber II/213. House Robber II.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
+    int rob_1(vector<int>& nums) {
         int n = nums.size() ;
-        vector<int> new_nums(n+1, 0) ;
-        new_nums[1] = nums[0] ;
-        for (int i = 2; i <= nums.size() ; i++) {
-            int max_val = max(new_nums[i-1], new_nums[i-2] + nums[i-1]) ;
-            new_nums[i] =  max_val;
-            if (i == 2 && max_val == new_nums[i-1] && nums[i-1] != 0){
-                n-- ;
-            }
+        vector<int> dp(n+1, 0) ;
+        dp[1] = nums[0] ;
+        for(int i= 2; i <= n; i++) {
+            dp[i] = max(dp[i-1], nums[i-1] + dp[i-2]) ;
         }
-        return max(new_nums[n-1], nums[nums.size()-1]) ;
+        return dp[n] ;
+    }
+    int rob(vector<int>& nums) {
+        if (nums.size() == 1) 
+            return nums[0] ;
+        vector<int> nums_1(nums.begin(), nums.end() - 1);
+        vector<int> nums_2(nums.begin() + 1, nums.end());
+        return max(rob_1(nums_1), rob_1(nums_2)) ;
     }
 };
