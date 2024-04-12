ee05835462c24fb2f01b31ba46e80b96e2020b6f
KhaledKammoun <khaledsfax03@gmail.com>
2024-04-12 10:15:13 +0100

    C++

--- Medium/5. Longest Palindromic Substring/5. Longest Palindromic Substring.cpp
+++ Medium/5. Longest Palindromic Substring/5. Longest Palindromic Substring.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
-    int expandAroundCenter(const string& s, int left, int right) {
+    string result = "";
+    void expandAroundCenter(const string& s, int left, int right) {
         while (left >= 0 && right < s.size() && s[left] == s[right]) {
-            left--;
-            right++;
+            right++ ;
+            left-- ;
         }
-        return right - left - 1;
+        if (right - left - 1 > result.size()) {
+            result = s.substr(left+1, right - left - 1) ;
+        }
+        return ;
     }
-
     string longestPalindrome(string s) {
-        int n = s.size();
-        int start = 0, maxLen = 0;
-
-        for (int i = 0; i < n; i++) {
-            int len1 = expandAroundCenter(s, i, i); // Odd-length palindrome
-            int len2 = expandAroundCenter(s, i, i + 1); // Even-length palindrome
-            int len = max(len1, len2);
-
-            if (len > maxLen) {
-                maxLen = len;
-                start = i - (len - 1) / 2;
-            }
+        int n = s.size() ;
+        for(int i = 0; i < n; i++) {
+            expandAroundCenter(s, i, i);
+            expandAroundCenter(s, i, i+1);
         }
-
-        return s.substr(start, maxLen);
+        return result ;
     }
 };
